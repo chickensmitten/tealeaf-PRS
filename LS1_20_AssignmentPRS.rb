200ea1f0d99740a9700c9b319047a542bc61de52
shengyeong <chia.shengyeong@gmail.com>
2014-09-09 15:19:58 +0800

    made changes to allow for inaccurate answers suggested by TA

--- LS1_20_AssignmentPRS.rb
+++ LS1_20_AssignmentPRS.rb
@@ -24,10 +24,10 @@ end
 
 def cont()
   prompt "Would you like to continue? Yes/No"
-  cont = gets.chomp
-  if cont == "Yes"
+  cont = gets.chomp.upcase
+  if cont == "Yes" || cont == "Y"
     select()
-  elsif cont == "No"
+  elsif cont == "No" || cont == "N"
     end_game
   else
     prompt "Invalid answer, please try again, yes or no?"
@@ -38,15 +38,15 @@ end
 def select()
   print "Play Paper, Rocks, Scissors!"
   prompt "Choose one: Paper/Rocks/Scissors "
-  choice = gets.chomp
+  choice = gets.chomp.upcase
 
-  if choice == "Paper"
+  if choice == "PAPER" || choice == "P" || choice == "PAPERS"
     prompt "You chose Paper"
     paper()
-  elsif choice == "Rocks"
+  elsif choice == "ROCKS" || choice == "R" || choice == "ROCK"
     prompt "You chose Rocks"
     rock()
-  elsif choice == "Scissors"
+  elsif choice == "SCISSORS" || choice == "S" || choice == "SCISSOR"
     prompt "You chose Scissors"
     scissors()
   else
@@ -56,15 +56,15 @@ def select()
 end
 
 def paper()
-  if Options.keys.sample == "Paper"
+  if Options.keys.sample == "PAPER"
     puts "Computer chose Paper"		
     puts "It is a tie."
     cont()
-  elsif Options.keys.sample == "Scissors"
+  elsif Options.keys.sample == "SCISSORS"
     puts "Computer chose Scissors"		
     puts "The computer wins"
     cont()
-  else Options.keys.sample == "Rocks"
+  else Options.keys.sample == "ROCKS"
     puts "Computer chose Rocks"
     puts "You win."
     cont()
@@ -72,15 +72,15 @@ def paper()
 end
 
 def rock()
-  if Options.keys.sample == "Rocks"
+  if Options.keys.sample == "ROCKS"
     puts "Computer chose Rocks"
     puts "It is a tie."
     cont()
-  elsif Options.keys.sample == "Paper"
+  elsif Options.keys.sample == "PAPER"
     puts "Computer chose Paper"	
     puts "The computer wins"
     cont()
-  else Options.keys.sample == "Scissors"
+  else Options.keys.sample == "SCISSORS"
     puts "Computer chose Scissors"	
     puts "You win."
     cont()
@@ -88,15 +88,15 @@ def rock()
 end
 
 def scissors()
-  if Options.keys.sample == "Scissors"
+  if Options.keys.sample == "SCISSORS"
     puts "Computer chose Scissors"			
     puts "It is a tie."
     cont()
-  elsif Options.keys.sample == "Rocks"
+  elsif Options.keys.sample == "ROCKS"
     puts "Computer chose Rocks"		
     puts "The computer wins"
     cont()
-  else Options.keys.sample == "Paper"
+  else Options.keys.sample == "PAPER"
     puts "Computer chose Paper"			
     puts "You win."
     cont()
